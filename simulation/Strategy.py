5b7fb75f1704604e7245d625c5d01ce3e71b1443
Julia Ducharme <122840323+jcharme@users.noreply.github.com>
2025-03-15 20:48:22 -0400

    Added Tit for two Tat strategy

--- simulation/Strategy.py
+++ simulation/Strategy.py
@@ -4,9 +4,11 @@ import random
 
 import Match
 
+
 class Action(Enum):
     COOPERATE = 0
     DEFECT = 1
+
 
 class Strategy:
     def __init__(self, match: Match):
@@ -17,13 +19,16 @@ class Strategy:
     def next_move(self) -> Action:
         pass
 
+
 class AlwaysCooperate(Strategy):
     def next_move(self):
         return Action.COOPERATE
-    
+
+
 class AlwaysDefect(Strategy):
     def next_move(self):
         return Action.DEFECT
+
 
 class TitForTat(Strategy):
     def next_move(self):
@@ -31,13 +36,28 @@ class TitForTat(Strategy):
             return Action.COOPERATE
         else:
             return self.opp_history[-1]
-        
+
+
+class TitFor2Tat(Strategy):
+    def next_move(self):
+        if len(self.opp_history) < 2:
+            return Action.COOPERATE
+        elif (
+            self.opp_history[-1] == Action.DEFECT
+            and self.opp_history[-2] == Action.DEFECT
+        ):
+            return Action.DEFECT
+        else:
+            return Action.COOPERATE
+
+
 class SuspiciousTitForTat(Strategy):
     def next_move(self):
         if len(self.opp_history) == 0:
             return Action.DEFECT
         else:
             return self.opp_history[-1]
+
 
 class Random(Strategy):
     def next_move(self):
