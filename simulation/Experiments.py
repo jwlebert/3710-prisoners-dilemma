b0109eacd6de9aade79fc61fbe1ea423873e1bc4
Julia Ducharme <122840323+jcharme@users.noreply.github.com>
2025-03-17 17:38:55 -0400

    updated file name

--- simulation/Experiments.py
+++ simulation/Experiments.py
@@ -1,7 +1,7 @@
 from algorithms.GeneticAlgorithm import GeneticAlgorithm
 import matplotlib.pyplot as plt
 
-def GeneticOptimization():
+def GeneticExperiments():
     ga = GeneticAlgorithm(pop_size=100, mutation_rate=0.001, memory_depth=3)
     ga.train(generations=100)
     genetic_strategy = ga.best_strategy()
